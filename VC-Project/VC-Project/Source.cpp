8f87a3dd750496345eb6bc0447a2292602332213
Eduardo Rebelo <100939320+EduardoR002@users.noreply.github.com>
2024-05-24 22:47:06 +0100

    First Functions

--- VC-Project/VC-Project/Source.cpp
+++ VC-Project/VC-Project/Source.cpp
@@ -104,25 +104,52 @@ int main(void) {
 		cv::putText(frame, str, cv::Point(20, 100), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(255, 255, 255), 1);
 
 
-		// Faça o seu código aqui...
-		/*
 		// Cria uma nova imagem IVC
-		IVC *image = vc_image_new(video.width, video.height, 3, 255);
-		// Copia dados de imagem da estrutura cv::Mat para uma estrutura IVC
-		memcpy(image->data, frame.data, video.width * video.height * 3);
-		// Executa uma função da nossa biblioteca vc
-		vc_rgb_get_green(image);
-		// Copia dados de imagem da estrutura IVC para uma estrutura cv::Mat
-		memcpy(frame.data, image->data, video.width * video.height * 3);
-		// Liberta a memória da imagem IVC que havia sido criada
-		vc_image_free(image);
-		*/
-		// +++++++++++++++++++++++++
+		IVC* image = vc_image_new(video.width, video.height, 3, 255);
+		IVC* image_2 = vc_image_new(video.width, video.height, 3, 255);
+		IVC* image_3 = vc_image_new(video.width, video.height, 3, 255);
+		IVC* image_4 = vc_image_new(video.width, video.height, 1, 255);
+		IVC* image_5 = vc_image_new(video.width, video.height, 1, 255);
 
-		/* Exibe a frame */
+		// Guarda a memória da imagem
+		memcpy(image_2->data, frame.data, video.width* video.height * 3);
+
+		// Converte a codificação de cores BGR (video original) para RGB
+		vc_convert_bgr_to_rgb(image_2, image_2);
+
+		// Converte a codificação de cores RGB para HSV
+		vc_rgb_to_hsv(image_2, image_3);
+
+		vc_hsv_segmentation(image_3, image_4, 25, 43, 32, 63, 44, 86);
+
+		// Abertura binária da imagem
+		vc_binary_open(image_4, image_5, 3, 3);
+
+		// Fechamento binário da imagem
+		vc_binary_close(image_5, image_4, 3, 3);
+
+		// Etiquetagem de blobs
+		OVC* blob = nullptr; 
+		int nblob = 0;
+		blob = vc_binary_blob_labelling(image_4, image_5, &nblob);
+
+		// Exibir a máscara resultante após abertura e fechamento
+		cv::Mat mask_mat(image_4->height, image_4->width, CV_8UC1, image_4->data); 
+		cv::imshow("Mask after binary operations", mask_mat); 
+
+		for (int i = 0; i < nblob; i++) {
+			int min_x = blob[i].x; 
+			int max_x = blob[i].x + blob[i].width; 
+			int min_y = blob[i].y; 
+			int max_y = blob[i].y + blob[i].height; 
+
+			cv::rectangle(frame, cv::Point(min_x, min_y), cv::Point(max_x, max_y), cv::Scalar(0, 255, 0), 2);
+		}
+
+		// Exibe a frame
 		cv::imshow("VC - VIDEO", frame);
 
-		/* Sai da aplicação, se o utilizador premir a tecla 'q' */
+		// Sai da aplicação, se o utilizador premir a tecla 'q'
 		key = cv::waitKey(1);
 	}
 
--- VC-Project/VC-Project/vc.h
+++ VC-Project/VC-Project/vc.h
@@ -1,1 +1,54 @@
-#pragma once
+﻿#define VC_DEBUG
+#ifndef VC_H
+#define VC_H
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                   ESTRUTURA DE UMA IMAGEM
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+typedef struct
+{
+	unsigned char* data;
+	int width, height;
+	int channels;	  // Binário/Cinzentos=1; RGB=3
+	int levels;		  // Binário=1; Cinzentos [1,255]; RGB [1,255]
+	int bytesperline; // width * channels
+} IVC;
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                   ESTRUTURA DE UM BLOB (OBJECTO)
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+typedef struct
+{
+	int x, y, width, height; // Caixa Delimitadora (Bounding Box)
+	int area;				 // �rea
+	int xc, yc;				 // Centro-de-massa
+	int perimeter;			 // Per�metro
+	int label;				 // Etiqueta
+} OVC;
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//                    PROTÓTIPOS DE FUNÇÕES
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+#define MAX(a, b) ((a) > (b) ? a : b)
+
+// FUNÇÕES: ALOCAR E LIBERTAR UMA IMAGEM
+IVC* vc_image_new(int width, int height, int channels, int levels);
+IVC* vc_image_free(IVC* image);
+
+int vc_convert_bgr_to_rgb(IVC* src, IVC* dst);
+int vc_rgb_to_hsv(IVC* src, IVC* dst);
+
+int vc_hsv_segmentation(IVC* src, IVC* dst, int hmin, int hmax, int smin, int smax, int vmin, int vmax);
+
+int vc_binary_dilate(IVC* src, IVC* dst, int kernel);
+int vc_binary_erosion(IVC* src, IVC* dst, int kernel);
+
+int vc_binary_open(IVC* src, IVC* dst, int kernelErosion, int kernelDilation);
+int vc_binary_close(IVC* src, IVC* dst, int kernelDilation, int kernelErosion);
+
+OVC* vc_binary_blob_labelling(IVC* src, IVC* dst, int* nlabels);
+int vc_binary_blob_info(IVC* src, OVC* blobs, int nblobs);
+#endif /* VC_H */
