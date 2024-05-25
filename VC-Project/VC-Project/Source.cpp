7fd3bb026955acf69d6489f34b3b850e7ac2de72
Eduardo Rebelo <100939320+EduardoR002@users.noreply.github.com>
2024-05-25 02:02:40 +0100

    Green and Blue colours

--- VC-Project/VC-Project/Source.cpp
+++ VC-Project/VC-Project/Source.cpp
@@ -86,6 +86,9 @@ int main(void) {
 		/* Verifica se conseguiu ler a frame */
 		if (frame.empty()) break;
 
+		cv::GaussianBlur(frame, frame, cv::Size(5, 5), 0);
+
+
 		/* Número da frame a processar */
 		video.nframe = (int)capture.get(cv::CAP_PROP_POS_FRAMES);
 
@@ -111,14 +114,23 @@ int main(void) {
 		IVC* image_4 = vc_image_new(video.width, video.height, 1, 255);
 		IVC* image_5 = vc_image_new(video.width, video.height, 1, 255);
 
+		IVC* red_segmented_image = vc_image_new(video.width, video.height, 1, 255); 
+		IVC* brown_segmented_image = vc_image_new(video.width, video.height, 1, 255); 
+		IVC* black_segmented_image = vc_image_new(video.width, video.height, 1, 255); 
+		IVC* orange_segmented_image = vc_image_new(video.width, video.height, 1, 255); 
+		IVC* green_segmented_image = vc_image_new(video.width, video.height, 1, 255);  
+		IVC* blue_segmented_image = vc_image_new(video.width, video.height, 1, 255); 
+
 		// Guarda a memória da imagem
 		memcpy(image_2->data, frame.data, video.width* video.height * 3);
 
 		// Converte a codificação de cores BGR (video original) para RGB
-		vc_convert_bgr_to_rgb(image_2, image_2);
+		vc_convert_bgr_to_rgb(image_2, image_2); 
 
 		// Converte a codificação de cores RGB para HSV
-		vc_rgb_to_hsv(image_2, image_3);
+		vc_rgb_to_hsv(image_2, image_3); 
+
+#pragma region Cor das Resistências
 
 		vc_hsv_segmentation(image_3, image_4, 25, 43, 32, 63, 44, 86);
 
@@ -129,22 +141,123 @@ int main(void) {
 		vc_binary_close(image_5, image_4, 3, 3);
 
 		// Etiquetagem de blobs
-		OVC* blob = nullptr; 
+		OVC* blob = nullptr;
 		int nblob = 0;
 		blob = vc_binary_blob_labelling(image_4, image_5, &nblob);
 
-		// Exibir a máscara resultante após abertura e fechamento
-		cv::Mat mask_mat(image_4->height, image_4->width, CV_8UC1, image_4->data); 
-		cv::imshow("Mask after binary operations", mask_mat); 
-
-		for (int i = 0; i < nblob; i++) {
-			int min_x = blob[i].x; 
-			int max_x = blob[i].x + blob[i].width; 
-			int min_y = blob[i].y; 
-			int max_y = blob[i].y + blob[i].height; 
-
-			cv::rectangle(frame, cv::Point(min_x, min_y), cv::Point(max_x, max_y), cv::Scalar(0, 255, 0), 2);
+		IVC* mask = vc_image_new(video.width, video.height, 1, 255);
+		
+		// Copia os pixels da imagem fechada para a máscara
+		for (int y = 0; y < video.height; y++) {
+			for (int x = 0; x < video.width; x++) {
+				int index = y * video.width + x;
+				// Se o pixel na imagem fechada for ativo (255), define o pixel na máscara como 255 (branco), caso contrário, define como 0 (preto)
+				if (image_4->data[index] == 255) {
+					mask->data[index] = 255;
+				}
+				else {
+					mask->data[index] = 0;
+				}
+			}
 		}
+
+		// Cria uma matriz OpenCV para exibir a máscara
+		cv::Mat mask_mat(mask->height, mask->width, CV_8UC1, mask->data);
+
+		// Mostra a máscara em uma janela separada
+		cv::imshow("Mask", mask_mat);
+
+#pragma endregion
+
+		//vc_hsv_segmentation(image_3, red_segmented_image, 25, 43, 32, 63, 44, 86); 
+		//vc_hsv_segmentation(image_3, brown_segmented_image, 25, 43, 32, 63, 44, 86); 
+		//vc_hsv_segmentation(image_3, black_segmented_image, 25, 43, 32, 63, 44, 86);
+		//vc_hsv_segmentation(image_3, orange_segmented_image, 25, 43, 32, 63, 44, 86);
+
+#pragma region Cor Verde
+
+		vc_hsv_segmentation(image_3, green_segmented_image, 85, 105, 33, 53, 35, 57);
+
+		// Abertura binária da imagem segmentada
+		IVC* green_opened_image = vc_image_new(video.width, video.height, 1, 255);
+		vc_binary_open(green_segmented_image, green_opened_image, 3, 3);
+
+		// Fechamento binário da imagem aberta
+		IVC* green_closed_image = vc_image_new(video.width, video.height, 1, 255);
+		vc_binary_close(green_opened_image, green_closed_image, 3, 3);
+
+		// Etiquetagem de blobs na imagem fechada
+		OVC* green_blob = nullptr;
+		int green_nblob = 0; 
+		green_blob = vc_binary_blob_labelling(green_closed_image, green_segmented_image, &green_nblob);
+
+		IVC* green_mask = vc_image_new(video.width, video.height, 1, 255);
+
+		// Copia os pixels da imagem segmentada para a máscara
+		for (int y = 0; y < video.height; y++) {
+			for (int x = 0; x < video.width; x++) {
+				int index = y * video.width + x;
+				// Se o pixel na imagem segmentada for azul (0), define o pixel na máscara como 255 (branco), caso contrário, define como 0 (preto)
+				if (green_closed_image->data[index] == 0) {
+					green_mask->data[index] = 0;
+				}
+				else {
+					green_mask->data[index] = 255;
+				}
+			}
+		}
+
+		// Cria uma matriz OpenCV para exibir a máscara
+		cv::Mat green_mask_mat(green_mask->height, green_mask->width, CV_8UC1, green_mask->data);
+
+		// Mostra a máscara em uma janela separada
+		cv::imshow("Blue Mask", green_mask_mat);
+
+#pragma endregion
+
+#pragma region Cor Azul
+
+		vc_hsv_segmentation(image_3, blue_segmented_image, 110, 200, 14, 45, 32, 45);
+
+		// Abertura binária da imagem segmentada
+		IVC* blue_opened_image = vc_image_new(video.width, video.height, 1, 255);
+		vc_binary_open(blue_segmented_image, blue_opened_image, 3, 3);
+
+		// Fechamento binário da imagem aberta
+		IVC* blue_closed_image = vc_image_new(video.width, video.height, 1, 255);
+		vc_binary_close(blue_opened_image, blue_closed_image, 3, 3);
+
+		// Etiquetagem de blobs na imagem fechada
+		OVC* blue_blob = nullptr;
+		int blue_nblob = 0;
+		blue_blob = vc_binary_blob_labelling(blue_closed_image, blue_segmented_image, &blue_nblob);
+
+		IVC* blue_mask = vc_image_new(video.width, video.height, 1, 255);
+
+		// Copia os pixels da imagem segmentada para a máscara
+		for (int y = 0; y < video.height; y++) {
+			for (int x = 0; x < video.width; x++) {
+				int index = y * video.width + x;
+				// Se o pixel na imagem segmentada for azul (0), define o pixel na máscara como 255 (branco), caso contrário, define como 0 (preto)
+				if (blue_closed_image->data[index] == 0) {
+					blue_mask->data[index] = 0;
+				}
+				else {
+					blue_mask->data[index] = 255;
+				}
+			}
+		}
+
+		// Cria uma matriz OpenCV para exibir a máscara
+		cv::Mat blue_mask_mat(blue_mask->height, blue_mask->width, CV_8UC1, blue_mask->data);
+
+		// Mostra a máscara em uma janela separada
+		cv::imshow("Blue Mask", blue_mask_mat);
+
+#pragma endregion
+
+
+	
 
 		// Exibe a frame
 		cv::imshow("VC - VIDEO", frame);
